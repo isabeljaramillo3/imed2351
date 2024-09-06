66be7599dba51ccf6444f9b34cdffcd811ed7c1e
isabeljaramillo3 <isabeljaramillo234@gmail.com>
2024-09-06 03:16:34 -0500

    app.js

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-var life = 100 ; // NUMBER
-
-life = life - 10; // 90
-
-life = life - 10; // 80
 
 
-var name = 'hello'; // STRING
+var name = 'hello'; // STRING VARIABLE
 
 var checkout = true; // BOOLEAN
 
-
 console.log(name);
 
+const globalVariable = 'hello again'; 
+console.log(globalVariable);
+// GLOBAL VARIABLE
+
+let life = 100;
+life = life - 40;
+console.log(life);
 
 
+{
+    const message = 'hi'
+    console.log(message);
+}
+{
+  const message = "hi again"
+    console.log(message)
+}
+// BLOCK SCOPE
 
+const user = {
+    name: 'John Doe',
+    age: 24,
+    married: false,
+    purchases: ["phone","car", "laptop"]
+};
 
+console.log(user.purchases)
 
-
-
-
-
-
-
-
-
+// OBJECT VARIABLE
 
 
 
