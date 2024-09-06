d6c9e7cd5ce2bb67e5e77278ee966b77dc477357
isabeljaramillo3 <isabeljaramillo234@gmail.com>
2024-09-06 00:31:08 -0500

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Did I do this correctly?");
+var life = 100 ; // NUMBER
+
+life = life - 10; // 90
+
+life = life - 10; // 80
+
+
+var name = 'hello'; // STRING
+
+var checkout = true; // BOOLEAN
+
+
+console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
