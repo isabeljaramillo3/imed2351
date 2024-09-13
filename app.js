98edde04288e4982cb5cdba50497a0d84e20a30a
isabeljaramillo3 <isabeljaramillo234@gmail.com>
2024-09-13 01:54:35 -0500

    app.js

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
+const name = "hello"
 
-
-var name = 'hello'; // STRING VARIABLE
-
-var checkout = true; // BOOLEAN
-
-console.log(name);
-
-const globalVariable = 'hello again'; 
-console.log(globalVariable);
-// GLOBAL VARIABLE
-
-let life = 100;
-life = life - 40;
-console.log(life);
-
-
-{
-    const message = 'hi'
-    console.log(message);
+//FUNCTION NUMB1 addition
+function hello1(parameter1, parameter2){
+   console.log(parameter1 + parameter2);
+ 
 }
-{
-  const message = "hi again"
-    console.log(message)
-}
-// BLOCK SCOPE
-
-const user = {
-    name: 'John Doe',
-    age: 24,
-    married: false,
-    purchases: ["phone","car", "laptop"]
-};
-
-console.log(user.purchases)
-
-// OBJECT VARIABLE
+hello1(25,49);
 
 
+//FUNCTION NUMB2 subtraction
+function hello2(parameter3, parameter4){
+    console.log(parameter3 - parameter4);
+  
+ }
+ hello2(185,58);
 
+ //FUNCTION NUMB3 division
+function hello3(parameter5, parameter6){
+    console.log(parameter5 / parameter6);
+  
+ }
+ hello3(10,2);
 
+ 
+//OBJECTS 
+
+ const person = {
+    firstName: "Mickey",
+    lastName: "Mouse",
+    age: 95,
+    isMouse: true,
+
+ }
+ 
+ console.log(person.firstName);
+ console.log(person.lastName);
+
+//METHOD
+
+let userName = "northwest vista";
+
+userName = userName.toUpperCase();
+
+console.log(userName);
