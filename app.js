d4e1ece3a63bf4861e69713360c9b82689bc7a84
isabeljaramillo3 <isabeljaramillo234@gmail.com>
2024-09-27 00:25:37 -0500

    app.js

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-//if condtional statement
-const age = 26
+// FOR LOOP
 
-if(age>28){
-   console.log("You are older than 28.")
-} else if (age < 25){
-   console.log("You are younger than 25.")
+const names = ["skibbi", "tolwe", "John", "Pork"];
+
+for(name of names){
+    console.log(name);
+    if (name === "Pork" ){
+        console.log("Mr.Pork is calling!");
+    }
 }
 
-   else{ console.log("You are older than 21.")}
+//WHILE LOOP
+let loading = 0;
+while(loading < 100){
+    console.log('Website is still loading');
 
+    loading++;
 
-//concatenated string
-const name = "IMED2351";
-const year = 2024;
-
-console.log(`Hello this class is called ${name} and the year is ${year}.`);
-
-const combined = name + year;
-console.log(combined);
-
-//switch statement
-let day = 2;
-let dayName;
-
-switch (day){
-    case 1:
-        dayName = "Monday";
-        break;
-    case 2:
-        dayName = "Tuesday";
-        break;
-    case 3:
-        dayName = "Wednesday";
-        break;
-    case 4:
-        dayName = "Thursday";
 }
-console.log(dayName); 
 
-//number method
-const PI = 3.1415
-   const num = PI.toFixed(3);
-      console.log(num);
+
+
+
